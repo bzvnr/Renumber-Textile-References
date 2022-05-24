34d6d73089bc3e98ebc322b8401961f81ce939e7
bzvnr <boazvenner2@gmail.com>
2022-05-24 19:24:32 -0400

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const config = require("./lib/config");
+const referenceSorter = require("./lib/referenceSorter");
+const referenceSorterInterface = require("./lib/referenceSorterInterface");
+
+modules.export = {
+  config,
+  referenceSorter,
+  referenceSorterInterface
+};
